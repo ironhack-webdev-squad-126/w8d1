b7c159774637fff6cf063f9abc2e3e00e230042f
mjarraya <mjarraya@student.42.fr>
2019-05-06 14:28:25 +0200

    get loggedin user

--- pm-app/src/App.js
+++ pm-app/src/App.js
@@ -5,6 +5,7 @@ import ProjectDetails from "./components/Projects/Details";
 import ProjectsList from "./components/Projects/List";
 import TaskDetails from "./components/Tasks/Details";
 import Signup from "./components/Auth/Signup";
+import { loggedin } from "./services/auth";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -19,6 +20,18 @@ class App extends React.Component {
       loggedIn: user
     });
   };
+
+  getUser = () => {
+    loggedin().then(user => {
+      this.setState({
+        loggedIn: user
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
 
   render() {
     return (
